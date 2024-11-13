dc2f7f27f1349efd6b2c4e51944e5ff9bc212503
mm-Aathik <mm.aathik@gmail.com>
2024-11-13 07:03:32 +0530

    payment-frontend

---
+++ app/payment/CheckoutForm.jsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React, { useState } from 'react';
+import { PaymentElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const CheckoutForm = ({ totalAmount, paymentId }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      console.error('Stripe.js or elements is not loaded');
+      return;
+    }
+
+    console.log('amount:', totalAmount);
+    console.log('paymentId:', paymentId);
+
+    const { error, paymentIntent } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: `http://localhost:3000/payment/success?amount=${totalAmount}&paymentId=${paymentId}`, // Redirect after successful payment
+      },
+    });
+
+    if (error) {
+      console.error('Payment error:', error);
+    } else if (paymentIntent && paymentIntent.status === 'succeeded') {
+      console.log('Payment successful!');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='gap-4'>
+      <PaymentElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
---
+++ app/payment/page.jsx
@@ -0,0 +1,91 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import Cart from './Cart';
+import CheckoutForm from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import './styles.css'; // Import the CSS file
+import axios from 'axios';
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+const PaymentPage = () => {
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [clientSecret, setClientSecret] = useState(null);
+  const [paymentId, setPaymentId] = useState(null);
+  const [showCheckout, setShowCheckout] = useState(false);
+
+  useEffect(() => {
+    if (totalAmount > 0) {
+      const fetchClientSecret = async () => {
+        try {
+          console.log('Fetching client secret...');
+          const response = await axios.post('http://127.0.0.1:8085/api/payments/create', {
+            amount: totalAmount * 100,
+            currency: 'usd',
+          });
+
+          console.log('Client secret fetched:', response.data.clientSecret);
+          setClientSecret(response.data.clientSecret);
+          setPaymentId(response.data.paymentId);
+        } catch (error) {
+          console.error('Error fetching client secret:', error);
+        }
+      };
+
+      fetchClientSecret();
+    }
+  }, [totalAmount]);
+
+  const handleTotalAmount = (amount) => {
+    console.log('Total amount in PaymentPage:', amount);
+    setTotalAmount(amount);
+  };
+
+  const handleCheckoutClick = () => {
+    if (clientSecret) {
+      setShowCheckout(true);
+    }
+  };
+
+  const handleBackToCartClick = () => {
+    setShowCheckout(false);
+  };
+
+  return (
+    <div className="payment-page">
+      <h1 className="title">Secure Payment Page</h1>
+      <div className="container">
+        {!showCheckout ? (
+          <>
+            <Cart onTotalAmountChange={handleTotalAmount} />
+            <button
+              className="checkout-button"
+              onClick={handleCheckoutClick}
+              disabled={!clientSecret}
+            >
+              Check Out
+            </button>
+          </>
+        ) : (
+          clientSecret && paymentId && (
+            <div className="checkout-section">
+              <Elements stripe={stripePromise} options={{ clientSecret }}>
+                <CheckoutForm totalAmount={totalAmount} paymentId={paymentId} />
+              </Elements>
+              <button
+                className="back-to-cart-button"
+                onClick={handleBackToCartClick}
+              >
+                Back to Cart
+              </button>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PaymentPage;
