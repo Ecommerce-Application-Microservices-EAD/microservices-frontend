680fce56f0dc5002235c5cb32be0857d8629a6a3
ChathuraAbeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-11-20 18:59:54 +0530

    Update - user profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -12,13 +12,26 @@ export default function ProfilePage() {
       'https://a.storyblok.com/f/191576/1200x800/a3640fdc4c/profile_picture_maker_before.webp',
   });
   const [isEditing, setIsEditing] = useState(false);
+  const [passwords, setPasswords] = useState({
+    currentPassword: '',
+    newPassword: '',
+    reEnterNewPassword: '',
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+    if (name in passwords) {
+      setPasswords((prevPasswords) => ({ ...prevPasswords, [name]: value }));
+    } else {
+      setUser((prevUser) => ({ ...prevUser, [name]: value }));
+    }
   };
 
   const handleSave = () => {
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
     setIsEditing(false);
   };
 
@@ -33,37 +46,71 @@ export default function ProfilePage() {
             className="w-full h-full object-cover rounded-full"
           />
         </div>
+        {/* value section */}
         {isEditing ? (
           <>
-            <div className="flex justify-between w-64 mb-6">
+            <div className="flex justify-between w-[30rem] mb-6">
               <span className="font-medium">Name:</span>
               <Input
                 type="text"
                 name="name"
                 value={user.name}
                 onChange={handleChange}
-                className="ml-2 h-6"
+                className="ml-2 h-6 w-64"
               />
             </div>
-            <div className="flex justify-between w-64 mb-6">
+            <div className="flex justify-between w-[30rem] mb-6">
               <span className="font-medium">Phone:</span>
               <Input
                 type="text"
                 name="phone"
                 value={user.phone}
                 onChange={handleChange}
-                className="ml-2 h-6"
+                className="ml-2 h-6 w-64"
               />
             </div>
-            <Button onClick={handleSave}>Save</Button>
+            <div className="flex justify-between w-[30rem] mb-6">
+              <span className="font-medium">Current Password:</span>
+              <Input
+                type="password"
+                name="currentPassword"
+                value={passwords.currentPassword}
+                onChange={handleChange}
+                className="ml-2 h-6 w-64"
+              />
+            </div>
+            <div className="flex justify-between w-[30rem] mb-6">
+              <span className="font-medium">New Password:</span>
+              <Input
+                type="password"
+                name="newPassword"
+                value={passwords.newPassword}
+                onChange={handleChange}
+                className="ml-2 h-6 w-64"
+              />
+            </div>
+            <div className="flex justify-between w-[30rem] mb-6">
+              <span className="font-medium">Re-enter New Password:</span>
+              <Input
+                type="password"
+                name="reEnterNewPassword"
+                value={passwords.reEnterNewPassword}
+                onChange={handleChange}
+                className="ml-2 h-6 w-64"
+              />
+            </div>
+            <div className="flex justify-between w-[30rem]">
+              <Button onClick={handleSave}>Save</Button>
+              <Button variant="outline" onClick={handleCancel}>Cancel</Button>
+            </div>
           </>
         ) : (
           <>
-            <div className="flex justify-between w-64 mb-6">
+            <div className="flex justify-between w-[30rem] mb-6">
               <span className="font-medium">Name:</span>
               <span className="text-right">{user.name}</span>
             </div>
-            <div className="flex justify-between w-64 mb-6">
+            <div className="flex justify-between w-[30rem] mb-6">
               <span className="font-medium">Phone:</span>
               <span className="text-right">{user.phone}</span>
             </div>
